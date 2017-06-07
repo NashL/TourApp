16cb0bf5b13b95998ca31ef3f02f4e42d2f5ec2d
Oscar Luza <onluza@gmail.com>
2017-06-07 12:13:23 -0500

    Initial angular-maps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AgmCoreModule } from 'angular2-google-maps/core';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyA39f2YfW79yNLJsJL3-pB-gnT2AbL5Us0'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
